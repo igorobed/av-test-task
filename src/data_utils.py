bbb19a8af8eb368db4192323a9e546296e55fefa
DungeonMaster3000 <igorobed@yandex.ru>
2022-06-27 00:26:31 +0300

    Add simple function for tokenization

---
+++ src/data_utils.py
@@ -0,0 +1,20 @@
+from typing import List
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
+from string import punctuation
+
+
+nltk.download("stopwords")
+en_stops = set(stopwords.words('english'))
+ru_stops = set(stopwords.words('russian'))
+stops = en_stops | ru_stops
+
+
+def simple_tokenizer(in_str: str) -> List[str]:
+    temp_tokenize = [item for item in wordpunct_tokenize(in_str) if item not in punctuation]
+    return [item for item in temp_tokenize if item not in stops]
+
+
+if __name__ == "__main__":
+    pass
